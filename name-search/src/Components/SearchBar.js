be78143b9486645609995fcff56652a6e1e9b6de
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-10 17:55:54 +0300

    Add handleChange to filter the imported names / map name

--- name-search/src/Components/SearchBar.js
+++ name-search/src/Components/SearchBar.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react'
+import importedNames from '../names.js'
 
 export class SearchBar extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
+            names: importedNames
 
         }
     }
+
+    handleChange = (event) => {
+        const inputText = event.target.value.toLowerCase()
+        // console.log(inputText)
+        const filteredNames = importedNames.filter(name => name.toLowerCase().includes(inputText))
+
+        this.setState({ names: filteredNames })
+    }
+
+
     render() {
         return (
             <div>
                 <h1>Name search</h1>
+                <p>matching names found: {this.state.names.length}</p>
                 <form>
-                    <input type="text" placeholder="Search names..." />
+                    <input type="text" placeholder="Search names..." onChange={(event) => this.handleChange(event)} />
                 </form>
                 <div style={{ margin: 'auto' }}>
-                    Names will be rendered here
+                    {this.state.names.map(name => {
+                        return <p key={name}>{name}</p>
+                    })}
                 </div>
             </div>
         )
